878a66dbd0e27dd911c6b5ee9c608f85684ad884
Gabriel Tolentino <gabrieltmar@icloud.com>
2023-08-28 20:43:14 -0300

    Teste

--- Template/ClientApp/src/app/counter/counter.component.ts
+++ Template/ClientApp/src/app/counter/counter.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-  public currentCount = 0;
+  public currentCount = 1;
   public numero = 1000;
 
   public incrementCounter() {
