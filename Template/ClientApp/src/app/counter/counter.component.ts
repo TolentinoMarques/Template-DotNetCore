98f93ecf92ea574109dd7180aa367eeff7d49531
Gabriel Tolentino <gabrieltmar@icloud.com>
2023-08-27 23:27:31 -0300

    Firts Deploy

---
+++ Template/ClientApp/src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-component',
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+  public currentCount = 0;
+  public numero = 1000;
+
+  public incrementCounter() {
+    this.currentCount = this.currentCount + this.numero;
+    if (this.currentCount > 5000) {
+      this.currentCount = 0;
+    }
+  }
+}
